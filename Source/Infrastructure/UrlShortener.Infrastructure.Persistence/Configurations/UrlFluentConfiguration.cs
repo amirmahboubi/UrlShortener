d83dd3ae80af1df3a647eb5203feec707cf00cb8
Pouya Mahboubi <MS2P@live.com>
2022-05-29 11:37:58 +0430

    :sparkles: feat(persistence): add UrlFluentConfiguration

---
+++ Source/Infrastructure/UrlShortener.Infrastructure.Persistence/Configurations/UrlFluentConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace UrlShortener.Infrastructure.Persistence.Configurations;
+
+internal class UrlFluentConfiguration : IEntityTypeConfiguration<Url>
+{
+    public void Configure(EntityTypeBuilder<Url> builder)
+    {
+        builder.Property(p => p.OriginalUrl).IsRequired();
+    }
+}
