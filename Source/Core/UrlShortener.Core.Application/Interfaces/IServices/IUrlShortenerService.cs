6b334619896ad04b26936a2b5c56f8220cbff4b6
Pouya Mahboubi <MS2P@live.com>
2022-05-29 14:09:28 +0430

    :sparkles: feat(application): add IUrlShortenerService

---
+++ Source/Core/UrlShortener.Core.Application/Interfaces/IServices/IUrlShortenerService.cs
@@ -0,0 +1,9 @@
+﻿using UrlShortener.Core.Application.Interfaces.DataTransferObjects.ServicesDTOs._Shared;
+
+namespace UrlShortener.Core.Application.Interfaces.IServices;
+
+public interface IUrlShortenerService
+{
+    Task<ResponseBase<string>> GenerateShortUrl(string originalUrl);
+    Task<ResponseBase<string>> GetOriginalUrl(string shortUrl);
+}
