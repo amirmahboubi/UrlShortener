952a832f49fa07f4667e24a6153a1c5eb43e63f4
Pouya Mahboubi <MS2P@live.com>
2022-05-29 11:31:34 +0430

    :sparkles: feat(persistence): add UrlShortenerContexts

---
+++ Source/Infrastructure/UrlShortener.Infrastructure.Persistence/Contexts/UrlShortenerContexts.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using UrlShortener.Core.Domain.Entities;
+
+namespace UrlShortener.Infrastructure.Persistence.Contexts;
+
+public class UrlShortenerContexts : DbContext
+{
+    public UrlShortenerContexts([NotNull] DbContextOptions options) : base(options) { }
+
+    public DbSet<Url> Urls { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		modelBuilder.ApplyConfigurationsFromAssembly(assembly: Assembly.GetExecutingAssembly());
+		base.OnModelCreating(modelBuilder);
+	}
+}
