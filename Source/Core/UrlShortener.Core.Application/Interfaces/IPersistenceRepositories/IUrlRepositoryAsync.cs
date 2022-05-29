7b381b2dbd9a5f17a7e5a6d14626e4a088b2fe57
Pouya Mahboubi <MS2P@live.com>
2022-05-29 12:23:57 +0430

    :sparkles: feat(application): add IUrlRepositoryAsync

---
+++ Source/Core/UrlShortener.Core.Application/Interfaces/IPersistenceRepositories/IUrlRepositoryAsync.cs
@@ -0,0 +1,8 @@
+﻿using UrlShortener.Core.Domain.Entities;
+
+namespace UrlShortener.Core.Application.Interfaces.IPersistenceRepositories;
+
+internal interface IUrlRepositoryAsync : IGenericRepositoryAsync<Url>
+{
+    Task<string?> GetOriginalUrlByShortUrl(string shortUrl);
+}
