83386de196ddfc5aeb7fc7200af812f887b2621a
Pouya Mahboubi <MS2P@live.com>
2022-05-29 11:51:50 +0430

    :sparkles: feat(application): add IGenericRepositoryAsync

---
+++ Source/Core/UrlShortener.Core.Application/Interfaces/IPersistenceRepositories/IGenericRepositoryAsync.cs
@@ -0,0 +1,7 @@
+﻿namespace UrlShortener.Core.Application.Interfaces.IPersistenceRepositories;
+
+public interface IGenericRepositoryAsync<T> where T : class
+{
+    Task<T> GetByIdAsync(int id);
+    Task<T> AddAsync(T entity);
+}
