b57a3c48fe601a2b6551df72fe532fca2dcbf157
Pouya Mahboubi <MS2P@live.com>
2022-05-29 16:09:00 +0430

    :sparkles: feat(application): add ServiceRegistration

---
+++ Source/Core/UrlShortener.Core.Application/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using UrlShortener.Core.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Core.Application.Interfaces.IServices;
+
+namespace UrlShortener.Core.Application;
+
+public static class ServiceRegistration
+{
+    public static void AddApplicationServices(this IServiceCollection services)
+    {
+        #region Services
+        services.AddScoped<IUrlShortenerService, UrlShortenerService>();
+        #endregion
+    }
+}
