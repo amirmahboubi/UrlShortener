3d70ac4129c5bacbe0fa20c136eaaf4622b02d8e
Pouya Mahboubi <MS2P@live.com>
2022-05-29 13:29:54 +0430

    :sparkles: feat(persistence): add UrlRepositoryAsync

---
+++ Source/Infrastructure/UrlShortener.Infrastructure.Persistence/Repositories/UrlRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Core.Domain.Entities;
+using UrlShortener.Infrastructure.Persistence.Contexts;
+using UrlShortener.Core.Application.Interfaces.IPersistenceRepositories;
+
+namespace UrlShortener.Infrastructure.Persistence.Repositories;
+
+internal class UrlRepositoryAsync : _GenericRepositoryAsync<Url>, IUrlRepositoryAsync
+{
+    private readonly DbSet<Url> _urls;
+
+    public UrlRepositoryAsync(UrlShortenerContexts dbContext) : base(dbContext) => 
+        _urls = dbContext.Set<Url>();
+
+    public async Task<Url?> GetByShortUrl(string shortUrl) =>
+        await _urls.FirstOrDefaultAsync(url => url.ShortUrl == shortUrl);
+}
