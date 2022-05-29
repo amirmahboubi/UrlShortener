fa8f19ba5c0e783e74a977958c3a87c1bd3c371a
Pouya Mahboubi <MS2P@live.com>
2022-05-29 11:57:56 +0430

    :sparkles: feat(persistence): add _GenericRepositoryAsync

---
+++ Source/Infrastructure/UrlShortener.Infrastructure.Persistence/Repositories/_GenericRepositoryAsync.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortener.Core.Application.Interfaces.IPersistenceRepositories;
+
+namespace UrlShortener.Infrastructure.Persistence.Repositories;
+
+internal class _GenericRepositoryAsync<T> : IGenericRepositoryAsync<T> where T : class
+{
+    private readonly DbContext _dbContext;
+
+    public _GenericRepositoryAsync(DbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<T> AddAsync(T entity)
+    {
+        await _dbContext.Set<T>().AddAsync(entity);
+        await _dbContext.SaveChangesAsync();
+        return entity;
+    }
+
+    public async Task<T> GetByIdAsync(int id) =>
+            await _dbContext.Set<T>().FindAsync(id);
+}
