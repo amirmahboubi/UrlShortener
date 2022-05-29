6bffbd4e648c3ae11c47ac2c8ae408d9da06976b
Pouya Mahboubi <MS2P@live.com>
2022-05-29 13:36:19 +0430

    :sparkles: feat(persistence): add ServiceRegistration

---
+++ Source/Infrastructure/UrlShortener.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UrlShortener.Infrastructure.Persistence.Contexts;
+using UrlShortener.Infrastructure.Persistence.Repositories;
+using UrlShortener.Core.Application.Interfaces.IPersistenceRepositories;
+
+namespace UrlShortener.Infrastructure.Persistence;
+
+public static class ServiceRegistration
+{
+    public static void AddInfrastructurePersistence(this IServiceCollection services, string connectionString)
+    {
+        #region DbContext
+        services.AddDbContext<UrlShortenerContexts>(c => c.UseSqlServer(connectionString));
+        #endregion
+
+        #region Repositories
+        services.AddTransient(typeof(IGenericRepositoryAsync<>), typeof(_GenericRepositoryAsync<>));
+        services.AddTransient<IUrlRepositoryAsync, UrlRepositoryAsync>();
+        #endregion
+    }
+}
