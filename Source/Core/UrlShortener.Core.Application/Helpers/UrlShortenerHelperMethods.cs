85f749847891cf370ce394594f9b16c6b2743fa8
Pouya Mahboubi <MS2P@live.com>
2022-05-29 15:32:44 +0430

    :sparkles: feat(application):  add UrlShortenerHelperMethods

---
+++ Source/Core/UrlShortener.Core.Application/Helpers/UrlShortenerHelperMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UrlShortener.Core.Application.Helpers
+{
+    internal class UrlShortenerHelperMethods
+    {
+        //proof against offensive words (removed 'a', 'e', 'i', 'o' and 'u')
+        private const string Alphabet = "0123456789bcdfghijklmnpqrstvwxyzBCDFGHIJKLMNPQRSTVWXYZ-_";
+        private static readonly int Base = Alphabet.Length;
+
+        /// <summary>
+        /// takes an ID and turns it into a short string
+        /// </summary>
+        /// <param name="Id">ID number</param>
+        /// <returns>short string</returns>
+        public static string Encode(long Id)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            while (Id > 0)
+            {
+                stringBuilder.Insert(0, Alphabet.ElementAt((Index)(Id % Base)));
+                Id = Id / Base;
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ Source/Core/UrlShortener.Core.Application/Services/UrlShortenerService.cs
@@ -0,0 +1,43 @@
+﻿using UrlShortener.Core.Application.Interfaces.IServices;
+using UrlShortener.Core.Application.DataTransferObjects.ServicesDTOs._Shared;
+using UrlShortener.Core.Application.Interfaces.IPersistenceRepositories;
+using UrlShortener.Core.Domain.Entities;
+using UrlShortener.Core.Application.Helpers;
+
+namespace UrlShortener.Core.Application.Services;
+
+internal class UrlShortenerService : IUrlShortenerService
+{
+    private readonly IUrlRepositoryAsync _urlRepositoryAsync;
+
+    public UrlShortenerService(IUrlRepositoryAsync urlRepositoryAsync) => 
+        _urlRepositoryAsync = urlRepositoryAsync;
+
+    public async Task<ResponseBase<string>> GenerateShortUrl(string originalUrl)
+    {
+        ResponseBase<string> response = new();
+        try
+        {
+            if (string.IsNullOrWhiteSpace(originalUrl))
+                response = ResponseBaseHelperMethods.BadRequestResponse<string>(message: "Url is null or empty");
+            else {
+                Url url = await _urlRepositoryAsync.AddAsync(entity: new Url()
+                {
+                    OriginalUrl = originalUrl,
+                    VisitsCount = 0,
+                });
+                response = ResponseBaseHelperMethods.SuccessResponse(url.ShortUrl);
+            }
+        }
+        catch (Exception ex)
+        {
+            response = ResponseBaseHelperMethods.ExceptionResponse<string>(ex);
+        }
+        return response;
+    }
+
+    public Task<ResponseBase<string>> GetOriginalUrl(string shortUrl)
+    {
+        throw new NotImplementedException();
+    }
+}
