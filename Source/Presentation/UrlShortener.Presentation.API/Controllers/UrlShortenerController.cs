9ddd4f0b4765d46875b851d481c9f2c38f86a9c6
Pouya Mahboubi <MS2P@live.com>
2022-05-29 16:41:17 +0430

    :sparkles: feat(api): add UrlShortenerController

---
+++ Source/Presentation/UrlShortener.Presentation.API/Controllers/UrlShortenerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using UrlShortener.Core.Application.DataTransferObjects.ServicesDTOs._Shared;
+using UrlShortener.Core.Application.Interfaces.IServices;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UrlShortener.Presentation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UrlShortenerController : ControllerBase
+    {
+        private readonly IUrlShortenerService _urlShortenerService;
+
+        public UrlShortenerController(IUrlShortenerService urlShortenerService)
+        {
+            _urlShortenerService = urlShortenerService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string shortUrl)
+        {
+            ResponseBase<string> response = await _urlShortenerService.GetOriginalUrl(shortUrl);
+            return StatusCode((int)(response?.Header?.StatusCode ?? HttpStatusCode.InternalServerError), response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string originalUrl)
+        {
+            ResponseBase<string> response = await _urlShortenerService.GenerateShortUrl(originalUrl);
+            return StatusCode((int)(response?.Header?.StatusCode ?? HttpStatusCode.InternalServerError), response);
+        }
+    }
+}
